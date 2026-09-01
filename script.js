a4e6b26fdcbf7624d2bcf98e6e2ccad43f7948e8
chalidanuanchanthuek-max <chalidanuanchanthuek@gmail.com>
2026-09-01 09:04:29 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 document.addEventListener('DOMContentLoaded',function () {
    document.getElementById(’year‘).innerHTML = new Date().getFullYear();
    console.log('Portfolio ของ Chalida พร้อมแล้ว!');
